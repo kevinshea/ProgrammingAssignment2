cf6960acc2e0a282f18444dc267cc2ad98a63d01
kevinshea <kshea421@kshea-mac.local>
2015-03-11 11:25:59 -0400

    Complete programming logic for cachematrix

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,19 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  my_inverse <- NULL
+  set <- function(y) {
+    x <<- y
+    my_inverse <<- NULL
+  }
+  
+  get <- function() x
+  set_inverse <- function(inverse) my_inverse <<- inverse
+  get_inverse <- function() my_inverse
+  
+  list(set = set, get = get, 
+       set_inverse = set_inverse,
+       get_inverse = get_inverse)
 }
 
 
@@ -12,4 +24,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  my_inverse <- x$get_inverse()
+  
+  if(!is.null(my_inverse)) {
+    message("getting cached data")
+    return(my_inverse)
+  }
+  
+  my_matrix <- x$get()
+  my_inverse <- solve(my_matrix)
+  x$set_inverse(my_inverse)
+  my_inverse
 }
